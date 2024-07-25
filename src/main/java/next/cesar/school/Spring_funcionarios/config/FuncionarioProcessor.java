3d0c2965f0af8abd19967ba96398b95758071479
IngridGermano <ingridgermano06@gmail.com>
2024-07-25 20:34:09 -0300

    Funcionario Processor editado

--- src/main/java/next/cesar/school/Spring_funcionarios/config/FuncionarioProcessor.java
+++ src/main/java/next/cesar/school/Spring_funcionarios/config/FuncionarioProcessor.java
@@ -13,33 +13,33 @@ public class FuncionarioProcessor implements ItemProcessor<Funcionario, Funciona
 	@Override
 	public Funcionario process(Funcionario item) throws Exception {
 
-		try {
-		if (item.getId().trim() == "") {
-		System.err.println("Nome inválido, por favor insira um nome válido.");
-		return null;
-		}
-		if (!Pattern.matches("[a-zA-Z]+", item.getId().trim())) {
-			System.err.println("Nome inválido, por favor insira um nome válido.");
-			return null;
-		}
-		if (Double.parseDouble(item.getSalario()) < 0 || item.getSalario() == null) {
-			System.err.println("Salário inválido, por favor insira um salário válido.");
-			return null;
-		}
-		if (Double.parseDouble(item.getAno()) < 0 || item.getAno() == null) {
-			System.err.println("Ano inválido, por favor insira um ano válido.");
-			return null;
-		}
-		if (item.getFuncao().trim() == "") {
-			System.err.println("Nome de função inválida, por favor insira uma função válida.");
-			return null;
-		}
+//		try {
+//		if (item.getId().trim() == "") {
+//		System.err.println("Nome inválido, por favor insira um nome válido.");
+//		return null;
+//		}
+//		if (!Pattern.matches("[a-zA-Z]+", item.getId().trim())) {
+//			System.err.println("Nome inválido, por favor insira um nome válido.");
+//			return null;
+//		}
+//		if (Double.parseDouble(item.getSalario()) < 0 || item.getSalario() == null) {
+//			System.err.println("Salário inválido, por favor insira um salário válido.");
+//			return null;
+//		}
+//		if (Double.parseDouble(item.getAno()) < 0 || item.getAno() == null) {
+//			System.err.println("Ano inválido, por favor insira um ano válido.");
+//			return null;
+//		}
+//		if (item.getFuncao().trim() == "") {
+//			System.err.println("Nome de função inválida, por favor insira uma função válida.");
+//			return null;
+//		}
 		System.out.println("Funcionário: " + item);
 		return item;
-		
-		} catch (NumberFormatException ex) {
-			System.err.println("");
-			return null;
-		}
+//		
+//		} catch (NumberFormatException ex) {
+//			System.err.println("");
+//			return null;
+//		}
 	}
 }
