6ddb67ece87d2b00f7e8d95623ebc085d052a83f
bartiraribeiro <you@example.com>
2024-07-25 23:24:59 -0300

    Última alterações de FuncionarioProcessor

--- src/main/java/next/cesar/school/Spring_funcionarios/config/FuncionarioProcessor.java
+++ src/main/java/next/cesar/school/Spring_funcionarios/config/FuncionarioProcessor.java
@@ -9,43 +9,19 @@ import org.springframework.batch.item.ItemProcessor;
 import next.cesar.school.Spring_funcionarios.entity.Funcionario;
 
 public class FuncionarioProcessor implements ItemProcessor<Funcionario, Funcionario> {
-
+	 
+	
+	
     @Override
     public Funcionario process(Funcionario item) throws Exception {
 
-//		try {
-//		if (item.getId().trim() == "") {
-//		System.err.println("Nome inválido, por favor insira um nome válido.");
-//		return null;
-//		}
-//		if (!Pattern.matches("[a-zA-Z]+", item.getId().trim())) {
-//			System.err.println("Nome inválido, por favor insira um nome válido.");
-//			return null;
-//		}
-//		if (Double.parseDouble(item.getSalario()) < 0 || item.getSalario() == null) {
-//			System.err.println("Salário inválido, por favor insira um salário válido.");
-//			return null;
-//		}
-//		if (Double.parseDouble(item.getAno()) < 0 || item.getAno() == null) {
-//			System.err.println("Ano inválido, por favor insira um ano válido.");
-//			return null;
-//		}
-//		if (item.getFuncao().trim() == "") {
-//			System.err.println("Nome de função inválida, por favor insira uma função válida.");
-//			return null;
-//		}
-//		
-//		} catch (NumberFormatException ex) {
-//			System.err.println("");
-//			return null;
-//		}
 	
         try {
             if (item.getId() == null || item.getId().trim().equals("")) {
                 System.err.println("Nome inválido, por favor insira um nome válido.");
                 return null;
             }
-           if (!Pattern.matches("[a-zA-Z- ÁáÉéÍíÓóÚú]+", item.getId().trim())) {
+           if (!Pattern.matches("[a-zA-Z- ÁáÉéÍíÓóÚú ÂâÊêÔô ÃãÕõ Çç ]+", item.getId().trim())) {
               System.err.println("Nome inválido, por favor insira um nome válido.");
               return null;
            }
@@ -53,6 +29,12 @@ public class FuncionarioProcessor implements ItemProcessor<Funcionario, Funciona
                System.err.println("Salário inválido, por favor insira um salário válido.");
               return null;
            }
+            String salarioSemPontos = removeDots(item.getSalario());
+            double salario = Double.parseDouble(salarioSemPontos);
+            if (salario < 0) {
+                System.err.println("Salário inválido, por favor insira um salário válido.");
+                return null;
+            }
            if (item.getAno() == null || Double.parseDouble(item.getAno()) < 0) {
               System.err.println("Ano inválido, por favor insira um ano válido.");
               return null;
@@ -68,6 +50,24 @@ public class FuncionarioProcessor implements ItemProcessor<Funcionario, Funciona
             System.err.println("Erro de formato de número: " + ex.getMessage());
             return null;
         }
+    }
+    static String removeDots(String teste) {
+        String result = "";
+        boolean dotFound = false;
+        for (int i = teste.length() - 1; i >= 0; i--) {
+            String temp = String.valueOf(teste.charAt(i));
+            if (!temp.equals(".")) {
+                if (!temp.equals("R") && !temp.equals("$") && !temp.equals(" ")) {
+                    result = temp + result;
+                }
+            } else {
+                if (!dotFound) {
+                    result = temp + result;
+                    dotFound = true;
+                }
+            }
+        }
+        return result;
     }
 }
 
